a81ab04b7439119ee29cbe1f2c9c1e467c17e738
CHRISTOPHER KELLY <christopherkelly@engi.network>
2022-08-11 08:14:48 -0700

    make it a real package

--- test/test_demo.py
+++ test/test_demo.py
@@ -1,6 +1,9 @@
+from engi_python_demo.calc import add, sub
+
+
 def test_ok():
-    assert 5 + 5 == 10
+    assert add(5, 5) == 10
 
 
 def test_fail():
-    assert 4 + 4 == 1
+    assert sub(10, 5) == 5
